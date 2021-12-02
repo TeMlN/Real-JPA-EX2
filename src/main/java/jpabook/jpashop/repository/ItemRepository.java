892fb8afd0c67aadb33ee953e75cb0dafa996916
taem1n1 <youman7953@naver.com>
2021-12-02 16:56:09 +0900

    실전 예제1 code

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) { //새롭게 등록된 item이라면? 바로 persist즉 수정이 아닌, 등록
+            em.persist(item);
+        } else { //새롭개 등록된 item이 아니라면 em.merge(item)작동
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+}
